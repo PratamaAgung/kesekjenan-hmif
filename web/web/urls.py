e5a22174fe70e278c563d24dc00cb849b90f2ccd
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 09:47:49 +0700

    change admin

--- web/web/urls.py
+++ web/web/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "Kesekjenan-HMIF Admin"
+admin.site.site_title = "Kesekjenan-HMIF Admin Portal"
+admin.site.index_title = "Welcome to Kesekjenan-HMIF"
