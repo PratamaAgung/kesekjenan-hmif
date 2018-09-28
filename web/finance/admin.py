14817280c37a8eb91324014759b73c4773c31000
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 14:33:11 +0700

    add search filter for description

--- web/finance/admin.py
+++ web/finance/admin.py
@@ -4,6 +4,7 @@ from .models import Income
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('income_date', 'description', 'income')
-    list_filter = ('income_date', 'description')
+    search_fields = ('description')
+    list_filter = ('income_date')
 
 admin.site.register(Income, IncomeAdmin)
