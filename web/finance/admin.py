c42932764cad05e8a0f15bbc3b9eab65954a8f74
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 14:54:49 +0700

    add range filter

--- web/finance/admin.py
+++ web/finance/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-
 from .models import Income
+from rangefilter.filter import DateRangeFilter
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('income_date', 'description', 'income')
     search_fields = ('description', )
-    list_filter = ('income_date', )
+    list_filter = (('income_date', DateRangeFilter), )
 
 admin.site.register(Income, IncomeAdmin)
--- web/web/settings.py
+++ web/web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'rangefilter',
     'finance.apps.FinanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
