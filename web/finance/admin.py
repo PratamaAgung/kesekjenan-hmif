546b4faa338bb68d2d1d597a9b43771901d8a2fc
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 14:42:59 +0700

    list filter syntax error

--- web/finance/admin.py
+++ web/finance/admin.py
@@ -5,6 +5,6 @@ from .models import Income
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('income_date', 'description', 'income')
     search_fields = ('description', )
-    list_filter = ('income_date')
+    list_filter = ('income_date', )
 
 admin.site.register(Income, IncomeAdmin)
