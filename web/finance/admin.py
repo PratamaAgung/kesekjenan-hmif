3a7bf22545e67f099ec16cc5796bfb62b1b854f5
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 14:41:14 +0700

    search field

--- web/finance/admin.py
+++ web/finance/admin.py
@@ -4,7 +4,7 @@ from .models import Income
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('income_date', 'description', 'income')
-    search_fields = ('description')
+    search_fields = ('description', )
     list_filter = ('income_date')
 
 admin.site.register(Income, IncomeAdmin)
