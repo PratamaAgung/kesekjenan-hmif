142e31ccc32829669350125a62fb83ef6fb8b4a7
PratamaAgung <aPratamaMia@gmail.com>
2018-09-28 13:35:56 +0700

    add income display for admin

--- web/finance/admin.py
+++ web/finance/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Income
 
-admin.site.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('income_date', 'description', 'income')
+
+admin.site.register(Income, IncomeAdmin)
--- web/finance/models.py
+++ web/finance/models.py
@@ -5,3 +5,6 @@ class Income(models.Model):
     income_date = models.DateField(default= date.today)
     description = models.CharField(max_length = 100)
     income = models.DecimalField(max_digits= 10, decimal_places= 3)
+
+    def __str__(self):
+        return self.description
