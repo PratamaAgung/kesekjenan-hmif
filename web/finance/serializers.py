17aabf1309abbf22e58a9cdbab3f141eda494b8d
PratamaAgung <aPratamaMia@gmail.com>
2018-09-29 09:16:38 +0700

    add all field to model serializer

--- web/finance/serializers.py
+++ web/finance/serializers.py
@@ -4,11 +4,13 @@ from .models import Income, Outcome, Reimbursement
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Income
-
+        fields = '__all__'
 class OutcomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Outcome
+        fields = '__all__'
 
 class ReimbursementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reimbursement
+        fields = '__all__'
