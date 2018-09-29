58bf06a0eb905794cfb6116c82372a472e44e688
PratamaAgung <aPratamaMia@gmail.com>
2018-09-29 09:23:07 +0700

    read only viewset

--- web/finance/views.py
+++ web/finance/views.py
@@ -4,14 +4,14 @@ from rest_framework import viewsets
 from .models import Income, Outcome, Reimbursement
 from .serializers import IncomeSerializer, OutcomeSerializer, ReimbursementSerializer
 
-class IncomeViewSet(viewsets.ModelViewSet):
+class IncomeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Income.objects.all()
     serializer_class = IncomeSerializer
 
-class OutcomeViewSet(viewsets.ModelViewSet):
+class OutcomeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Outcome.objects.all()
     serializer_class = OutcomeSerializer
 
-class ReimbursementViewSet(viewsets.ModelViewSet):
+class ReimbursementViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Reimbursement.objects.all()
     serializer_class = ReimbursementSerializer
