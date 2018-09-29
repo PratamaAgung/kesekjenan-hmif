79d51ff8b88a9c1515f2d6a1951d9352dab6db77
PratamaAgung <aPratamaMia@gmail.com>
2018-09-29 12:59:22 +0700

    change queryset in income

--- web/finance/views.py
+++ web/finance/views.py
@@ -5,8 +5,22 @@ from .models import Income, Outcome, Reimbursement
 from .serializers import IncomeSerializer, OutcomeSerializer, ReimbursementSerializer
 
 class IncomeViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Income.objects.all()
     serializer_class = IncomeSerializer
+
+    def get_queryset(self):
+        list_for_each = self.request.query_params.get('each')
+        from_date = self.request.query_params.get('from')
+        until_date = self.request.query_params.get('until')
+
+        if (list_for_each is None):
+            list_for_each = 'day'
+
+        if (from_date is not None and until_date is not None):
+            queryset = Income.objects.filter(income_date_lte= until_date, income_date_gte= from_date).values_list(list_for_each).annotate(total_income = Count('income'))
+        else :
+            queryset = Income.objects.all().values_list(list_for_each).annotate(total_income = Count('income'))
+
+        return queryset
 
 class OutcomeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Outcome.objects.all()
