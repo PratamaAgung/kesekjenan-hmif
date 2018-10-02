cc01438b707b717e904b452bb198644b572c4a4d
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 19:59:56 +0700

    try using list api view

--- web/finance/views.py
+++ web/finance/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from .models import Income, Outcome, Reimbursement
 from .serializers import IncomeSerializer, OutcomeSerializer, ReimbursementSerializer
 
-class IncomeViewSet(viewsets.ReadOnlyModelViewSet):
+class IncomeViewSet(generics.ListAPIView):
     serializer_class = IncomeSerializer
 
-    list_for_each = request.query_params.get('each')
-    from_date = request.query_params.get('from')
-    until_date = request.query_params.get('until')
+    def get_queryset(self):
+        list_for_each = self.request.query_params.get('each')
+        from_date = self.request.query_params.get('from')
+        until_date = self.request.query_params.get('until')
 
-    if (list_for_each is None):
-        list_for_each = 'day'
+        if (list_for_each is None):
+            list_for_each = 'day'
 
-    if (from_date is not None and until_date is not None):
-        queryset = Income.objects.filter(income_date_lte= until_date, income_date_gte= from_date).values_list(list_for_each).annotate(total_income = Count('income'))
-    else :
-        queryset = Income.objects.all().values_list(list_for_each).annotate(total_income = Count('income'))
+        if (from_date is not None and until_date is not None):
+            queryset = Income.objects.filter(income_date_lte= until_date, income_date_gte= from_date).values_list(list_for_each).annotate(total_income = Count('income'))
+        else :
+            queryset = Income.objects.all().values_list(list_for_each).annotate(total_income = Count('income'))
+
+        return queryset
 
 class OutcomeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Outcome.objects.all()
