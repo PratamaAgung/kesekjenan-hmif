afce185d073860a3ab6ed942df20e8b989d239eb
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 20:04:52 +0700

    try using list api view

--- web/finance/urls.py
+++ web/finance/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import IncomeViewSet, OutcomeViewSet, ReimbursementViewSet
 
 router = routers.DefaultRouter()
-router.register(r'income', IncomeViewSet)
+router.register(r'income', IncomeViewSet.as_view())
 router.register(r'outcome', OutcomeViewSet)
 router.register(r'reimbursement', ReimbursementViewSet)
 
--- web/finance/views.py
+++ web/finance/views.py
@@ -11,14 +11,15 @@ class IncomeViewSet(generics.ListAPIView):
         list_for_each = self.request.query_params.get('each')
         from_date = self.request.query_params.get('from')
         until_date = self.request.query_params.get('until')
+        queryset = Income.objects.all()
 
         if (list_for_each is None):
             list_for_each = 'day'
 
         if (from_date is not None and until_date is not None):
-            queryset = Income.objects.filter(income_date_lte= until_date, income_date_gte= from_date).values_list(list_for_each).annotate(total_income = Count('income'))
+            queryset = queryset.filter(income_date_lte= until_date, income_date_gte= from_date).values_list(list_for_each).annotate(total_income = Count('income'))
         else :
-            queryset = Income.objects.all().values_list(list_for_each).annotate(total_income = Count('income'))
+            queryset = queryset.values_list(list_for_each).annotate(total_income = Count('income'))
 
         return queryset
 
