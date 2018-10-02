769af0f148b864b78a70c72f2eb0d00054e34b1f
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 21:45:18 +0700

    fix naming error

--- web/finance/serializers.py
+++ web/finance/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 from .models import Income, Outcome, Reimbursement
 
 class IncomeSerializer(serializers.ModelSerializer):
+    month = serializers.DateField()
+    total_income = serializers.DecimalField()
+    
     class Meta:
         model = Income
         fields = '__all__'
-        
+
 class OutcomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Outcome
