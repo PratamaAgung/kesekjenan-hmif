5702817a401d6f54d9f40ad6be91f6fd5a12321d
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 20:12:08 +0700

    try using list api view

--- web/finance/urls.py
+++ web/finance/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import IncomeViewSet, OutcomeViewSet, ReimbursementViewSet
 
 router = routers.DefaultRouter()
-router.register(r'income', IncomeViewSet.as_view(), base_name = 'income-list')
+router.register(r'income', IncomeViewSet, base_name = 'income-list')
 router.register(r'outcome', OutcomeViewSet)
 router.register(r'reimbursement', ReimbursementViewSet)
 
