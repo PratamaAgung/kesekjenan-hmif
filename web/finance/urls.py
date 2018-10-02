edf60590c6357efe6894436f75f6a0b53f498229
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 20:49:49 +0700

    fix syntax error

--- web/finance/urls.py
+++ web/finance/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from .views import IncomeViewSet, OutcomeViewSet, ReimbursementViewSet
 
-router = routers.DefaultRouter())
+router = routers.DefaultRouter()
 router.register(r'outcome', OutcomeViewSet)
 router.register(r'reimbursement', ReimbursementViewSet)
 
