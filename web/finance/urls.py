4575c61db761aee62eb4d23eb4d7a48dd9bf06eb
PratamaAgung <aPratamaMia@gmail.com>
2018-10-02 20:47:29 +0700

    try using list api view

--- web/finance/urls.py
+++ web/finance/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
 from .views import IncomeViewSet, OutcomeViewSet, ReimbursementViewSet
 
-router = routers.DefaultRouter()
-router.register(r'income', IncomeViewSet, base_name = 'income-list')
+router = routers.DefaultRouter())
 router.register(r'outcome', OutcomeViewSet)
 router.register(r'reimbursement', ReimbursementViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'^income/$', IncomeViewSet.as_view(), name = 'income-list'),
+    url(r'^', include(router.urls)),
+]
