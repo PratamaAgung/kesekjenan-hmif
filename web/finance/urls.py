b10bdb6e6287a388623dc957e9f7d9642ae92fea
PratamaAgung <aPratamaMia@gmail.com>
2018-10-03 08:03:14 +0700

    register new api root

--- web/finance/urls.py
+++ web/finance/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import IncomeSummaryViewSet, OutcomeViewSet, ReimbursementViewSet
+from .views import IncomeSummaryViewSet, OutcomeViewSet, ReimbursementViewSet, api_root
 
 router = routers.DefaultRouter()
 router.register(r'outcome', OutcomeViewSet)
@@ -9,4 +9,5 @@ router.register(r'reimbursement', ReimbursementViewSet)
 urlpatterns = [
     url(r'^income-summary/$', IncomeSummaryViewSet.as_view(), name = 'income-summary-list'),
     url(r'^', include(router.urls)),
+    url(r'', include(api_root))
 ]
